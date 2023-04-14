92e3ca2c4b79d7de8aca5a2c44765fb3d21b5733
hftamayo <hftamayo@gmail.com>
2023-04-14 08:20:53 -0600

    adding controls for data input part2 and business logic for adding a user

--- app/src/main/java/com/hftamayo/kotlincrudroom/AddActivity.kt
+++ app/src/main/java/com/hftamayo/kotlincrudroom/AddActivity.kt
@@ -2,10 +2,33 @@ package com.hftamayo.kotlincrudroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.hftamayo.kotlincrudroom.databinding.ActivityAddBinding
+import kotlinx.coroutines.launch
 
 class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAddUser.setOnClickListener{ addUser() }
+    }
+
+    private fun addUser(){
+        val firstName = binding.edFirstName.text.toString()
+        val lastName = binding.edLastName.text.toString()
+
+        lifecycleScope.launch {
+            val user = User(firstName = firstName, lastName = lastName)
+            AppDatabase( this@AddActivity).getUserDao().addUser(user)
+            finish()
+        }
+
+
+
+
+
     }
 }
