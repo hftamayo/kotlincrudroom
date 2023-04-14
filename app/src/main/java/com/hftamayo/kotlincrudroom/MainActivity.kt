05d6aadbdcbb823be92131c618bccc140f9ebd47
hftamayo <hftamayo@gmail.com>
2023-04-14 09:35:06 -0600

    first version of the app tested on emulator

--- app/src/main/java/com/hftamayo/kotlincrudroom/MainActivity.kt
+++ app/src/main/java/com/hftamayo/kotlincrudroom/MainActivity.kt
@@ -2,8 +2,11 @@ package com.hftamayo.kotlincrudroom
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.hftamayo.kotlincrudroom.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity: AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -16,6 +19,15 @@ class MainActivity: AppCompatActivity() {
         binding.btnAdd.setOnClickListener {
             val intent = Intent(this, AddActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onResume(){
+        super.onResume()
+        lifecycleScope.launch {
+            val userList = AppDatabase(this@MainActivity).getUserDao().getAllUser()
+            Log.e("from MainActivity", "onResume event length of users list: ${userList.size}")
+
         }
     }
 }
