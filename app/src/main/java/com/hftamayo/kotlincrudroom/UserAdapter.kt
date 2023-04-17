5928c6954341ee46ab2a5d34fd9e2674b3bcba1c
hftamayo <hftamayo@gmail.com>
2023-04-17 08:53:58 -0600

    adding an activity for visualize user's list

---
+++ app/src/main/java/com/hftamayo/kotlincrudroom/UserAdapter.kt
@@ -0,0 +1,11 @@
+package com.hftamayo.kotlincrudroom
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class UserAdapter {
+
+    class UserViewMolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+}
