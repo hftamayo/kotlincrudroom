d041e944b733b5d4caa359fea65154bcd5f1170f
hftamayo <hftamayo@gmail.com>
2023-04-17 09:17:46 -0600

    business logic for set and display user datalist

--- app/src/main/java/com/hftamayo/kotlincrudroom/UserAdapter.kt
+++ app/src/main/java/com/hftamayo/kotlincrudroom/UserAdapter.kt
@@ -1,11 +1,41 @@
 package com.hftamayo.kotlincrudroom
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
+import org.w3c.dom.Text
 
-class UserAdapter {
+class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewMolder>() {
+    private var list = mutableListOf<User>()
 
-    class UserViewMolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewMolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_item_user_view_holder, parent, false)
+
+        return UserViewMolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewMolder, position: Int) {
+        val user = list[position]
+        holder.tvFirstName.text = user.firstName
+        holder.tvLastName.text = user.lastName
+    }
+
+    override fun getItemCount() = list.size
+
+    fun setData(data: List<User>){
+        list.apply {
+            clear()
+            addAll(data)
+        }
+    }
+
+    class UserViewMolder(itemView: View): ViewHolder(itemView){
+        val tvFirstName: TextView = itemView.findViewById(R.id.tv_first_name)
+        val tvLastName: TextView = itemView.findViewById(R.id.tv_last_name)
 
     }
+
 }
