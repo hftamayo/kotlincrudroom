29c211413fa104085f79c7eab594c78743b22944
hftamayo <hftamayo@gmail.com>
2023-04-18 08:57:17 -0600

    adding serializable to the model

--- app/src/main/java/com/hftamayo/kotlincrudroom/User.kt
+++ app/src/main/java/com/hftamayo/kotlincrudroom/User.kt
@@ -2,11 +2,12 @@ package com.hftamayo.kotlincrudroom
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity
 data class User (
     var firstName: String = "",
     var lastName: String = ""
-) {
+): Serializable {
     @PrimaryKey(autoGenerate = true) var id: Int = 0
 }
